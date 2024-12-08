25999bf0255737837facf777d4378b14fb9af0aa
James Absalem Lintu <jamesabsalem@gmail.com>
2024-12-08 23:05:07 +0600

    added CORS

---
+++ CompanyEmployee/Extensions/ServiceExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace CompanyEmployee.Extensions;
+
+public static class ServiceExtensions
+{
+    // CORS extensions
+    public static void ConfigureCors(this IServiceCollection services)
+    {
+        services.AddCors(option =>
+        {
+            option.AddPolicy("CorsPolicy", builder =>
+                builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+        });
+    }
+
+    // IIS extensions
+    public static void ConfigureIISIntegration(this IServiceCollection services)
+    {
+        services.Configure<IISOptions>(option => { });
+    }
+}
--- CompanyEmployee/Program.cs
+++ CompanyEmployee/Program.cs
@@ -1,10 +1,32 @@
+using CompanyEmployee.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
 
-var app = builder.Build();
+builder.Services.AddControllers();
 
 // Configure the HTTP request pipeline.
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+else
+    app.UseHsts();
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+
+app.UseCors("CorsPolicy");
+app.UseAuthorization();
+app.MapControllers();
 
 
 
